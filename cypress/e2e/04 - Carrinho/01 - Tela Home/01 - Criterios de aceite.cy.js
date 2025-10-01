1a835b767d870143df7549b1f2554b2ead3ba603
josafagonzaga <josafa.gonzaga@hotmail.com>
2025-10-01 19:18:35 -0300

    0101/2025 - star bugs tela home

--- cypress/e2e/04 - Carrinho/01 - Tela Home/01 - Criterios de aceite.cy.js
+++ cypress/e2e/04 - Carrinho/01 - Tela Home/01 - Criterios de aceite.cy.js
@@ -1,3 +1,4 @@
+//Projeto > tela > criterios de aceite
 context('Projeto - Star Bugs', () => 
 {
 	let contexto = 1;
@@ -9,6 +10,7 @@ context('Projeto - Star Bugs', () =>
 		context(`${contexto} - Critérios de aceite.`, () => 
 		{
 			let complemento = 1;
+			
 			describe(`${cenario} - Validações Visuais.`, () => 
 			{
 				
@@ -22,7 +24,7 @@ context('Projeto - Star Bugs', () =>
 					cy.clearAllSessionStorage();
 				});
 
-				describe.only(`${cenario} - Cabeçalho.`, () => {
+				describe(`${cenario} - Cabeçalho.`, () => {
 					
 					it(`${teste}.${complemento} - Validar acesso ao projeto.`, () => 
 					{
@@ -34,66 +36,117 @@ context('Projeto - Star Bugs', () =>
 					});
 
 					it(`${teste}.${complemento} - Validar titulo`, () => 
-				{
+					{
 					//Dado que eu acesso o ssistema 'starbugs'
 					//Quando estiver acessando a tela home
-					//Entao eu valido o titulo
 					cy.waitUntil(() =>
 						cy.get('[src="/assets/logo.bd07c5b3.svg"]')
 							.should('be.visible')
-					);
+						);
+					//Entao eu valido o titulo
+						cy.get('[src="/assets/logo.bd07c5b3.svg"]')
+							.should('be.visible')
+					});
 
-					
-					
-					
-				});
+					it(`${teste}.${complemento} - Validar sub-titulo`, () => 
+					{
+					//Dado que eu acesso o ssistema 'starbugs'
+					//Quando estiver acessando a tela home
+					//Entao eu valido o sub-titulo
+
+					//visivel
+					cy.get('button.gocart > svg')
+						//.find('svg')
+      					.should('be.visible')
+						//contem o texto especifico
+						cy.get('button.gocart')
+							.should('contain.text', 'Feito com')
+							.and('contain.text', 'por Papito!')
+							//SVG visivel, assegura que pegou o elemento certo mesmo tendo outros SVG	
+							cy.get('button.gocart').within(() => {
+									cy.get('svg')
+									.should('be.visible');
+									});
+									//css
+									cy.get('button.gocart')
+										.should('be.visible')
+										.and('have.css', 'display', 'flex')        // ex: inline-flex, flex, block, etc.
+										.and('have.css', 'background-color', 'rgb(212, 233, 226)')  // cor do texto
+										.and('have.css', 'color', 'rgb(30, 57, 50)') // cor de fundo
+										.and('have.css', 'font-size','14px')        // tamanho da fonte
+					});
 				});
 
 				describe(`${cenario} - Seçao 01 - Banner.`, () => {
+					it(`${teste}.${complemento} - Validar titulo`, () => 
+					{
+					//Dado que estou na tela home
+					//Quando visualizar o banner
+					cy.get('section >h1.mrPjC')
+					//Entao eu valido o titulo
+						.should('be.visible')
+						.and('have.text','Bora tomar um café? Nós levamos até você! ')
+					});
+
+					it(`${teste}.${complemento} - Validar sub-titulo`, () => 
+					{
+					//Dado que estou na tela home
+					//Quando visualizar o banner
+					cy.get('h3.subtitle')
+					//Entao eu valido o sub-titulo
+						.should('be.visible')
+						.and('have.text','O melhor café de são paulo, porque bug somente no nome.')
+					});
+
+					it(`${teste}.${complemento} - Validar informações`, () => 
+					{
+					//Dado que estou na tela home
+					//Quando visualizar o banner
+					cy.get('.sc-kgTSHT')
+						.should('be.visible')
+					//Entao eu valido as informações
+					//Frase 1
+					cy.get('.sc-iveFHk > :nth-child(1)')
+						.should('be.visible')
+						.and('have.text','Pague somente na entrega')
+						.find('svg')
+						.and('be.visible');
+							//Frase 2
+							cy.get('.sc-iveFHk > :nth-child(2)')
+								.should('be.visible')
+								.and('have.text','Embalagens reutilizáveis')
+								.find('svg')
+								.and('be.visible');
+									//Frase 3
+									cy.get('.sc-iveFHk > :nth-child(3)')
+										.should('be.visible')
+										.and('have.text','Entrega rápida e sustentável')
+										.find('svg')
+										.and('be.visible');
+										//Frase 3
+										cy.get('.sc-iveFHk > :nth-child(4)')
+											.should('be.visible')
+											.and('have.text','Cafés orgânicos e moídos na hora')
+											.find('svg')
+											.and('be.visible');
+					});
+
+					it(`${teste}.${complemento} - Validar imagem do lado direito`, () => 
+					{
+					//Dado que estou na tela home
+					//Quando visualizar o banner
+					cy.get('.hero')
+					//Entao eu valido a imagem da empresa sendo mostrada do lado direito
+						.should('be.visible')
+					});
+
 					
 				});
 
 				describe(`${cenario} - Seção 02 - Produtos mais vendidos.`, () => {
 					
 				});
-
-				it(`${teste}.${complemento} - Valida layout`, () => 
-				{
-					//Dado que eu acesso o ssistema 'starbugs'
-					//Quando estiver acessando a tela home
-					cy.waitUntil(() =>
-						cy.get('[src="/assets/logo.bd07c5b3.svg"]')
-							.should('be.visible')
-					);
-
-					/*Então o sistema deverá apresentar
-						*Logotipo
-						*Título
-						*Sub-título lado direito
-						*Slogan da pagina
-						*imagem lado direito
-						*Avisos
-						*Produtos mais vendidos
-						*cesta de produtos
-							-Img do produto
-							-Descrição
-							-Preço
-							-Botao comprar
-					*/
-					cy.get('.sc-dkrFOg')
-						.should('be.visible')
-						//.and('have.text','Feito com ')
-						.and('have.css', 'background-color', 'rgb(212, 233, 226)')
-						cy.get('h1')
-							.should('be.visible')
-							.and('contain.text', 'Bora tomar um café?')
-							.and('contain.text', 'Nós levamos até você!');
-					
-					
-				});
 			});
 		});
 	});
-
-	
 });
